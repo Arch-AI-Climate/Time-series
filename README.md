037d11a3b940844a9aa52858a74386b708066ac8
Jacky Liu <Jacky35liu@gmail.com>
2022-08-13 13:09:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# time-series
+ Persist time series data
---
+++ src/main/java/iotinfra/timeseries/TimeSeriesApplication.java
@@ -0,0 +1,13 @@
+package iotinfra.timeseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSeriesApplication.class, args);
+	}
+
+}
